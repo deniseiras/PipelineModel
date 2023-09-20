9c18ff56a4083d88fd31dc830d4a1e64bceda59e
deniseiras <denis.eiras@gmail.com>
2023-09-20 18:40:48 -0300

    creating dist  in install

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ ENV_NAME="${PRJ_NAME}_env"
 
 python3.8 -m venv ${ENV_NAME}
 source ${ENV_NAME}/bin/activate
+python setup.py sdist
 pip install ./dist/${PRJ_NAME}-${VERSION}.tar.gz
+
+echo -e "Now execute to load environment:\nsource ${ENV_NAME}/bin/activate"
